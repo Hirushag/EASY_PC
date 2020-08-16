1b4023ddbc0e3cb98af9b89e101ce6d0bbc7f6f3
Sandini Pitawala <sandinipitawala1@gmail.com>
2020-08-17 02:54:18 +0530

    My first Commit

---
+++ app/src/main/java/com/example/easypc/VUDlaptopsDell.java
@@ -0,0 +1,14 @@
+package com.example.easypc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VUDlaptopsDell extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_v_u_dlaptops_dell);
+    }
+}
