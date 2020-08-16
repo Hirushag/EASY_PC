ca0f80a85b1fbd956e53da213112acb60d4823de
Hirushag <hirushagagana@gmail.com>
2020-08-17 01:02:56 +0530

    Initial commit

---
+++ app/src/main/java/com/example/easypc/AddPcParts.java
@@ -0,0 +1,14 @@
+package com.example.easypc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class AddPcParts extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_pc_parts);
+    }
+}
---
+++ app/src/main/java/com/example/easypc/SparePatrs.java
@@ -0,0 +1,14 @@
+package com.example.easypc;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SparePatrs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_spare_patrs);
+    }
+}
